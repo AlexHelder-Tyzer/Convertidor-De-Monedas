9a41172c03ce8bf769d3978cc2ad502fb9f3c58d
tyzer <helder.huancara@gmail.com>
2024-06-24 22:19:51 -0500

    Importacion de la biblioteca Gson

--- convertidor-de-monedas/src/Main.java
+++ convertidor-de-monedas/src/Main.java
@@ -1,6 +1,12 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Esto es un test");
+
+        // prueba de gson correct
+        Gson gson = new Gson();
     }
 }
